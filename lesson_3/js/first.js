13f94807cef7a915e282255bf2cc76fa20e233c5
SuperVladK <128508632+SuperVladK@users.noreply.github.com>
2024-02-29 17:30:29 +0200

    Add files via upload

---
+++ lesson_3/js/first.js
@@ -0,0 +1,7 @@
+"use strict"
+let userName = prompt(`Your name`, `Your Majesty`)
+document.write(`Hello! ${userName} the Great is here! `) 
+let firstNumber = parseInt(prompt(`First Num`))
+let secondNumber = parseInt(prompt(`Second Num`))
+let s = firstNumber + secondNumber 
+document.write(`Your rate is ${s}!`) 
